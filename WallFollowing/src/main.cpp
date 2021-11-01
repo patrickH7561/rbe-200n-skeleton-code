fff084000d062aa62d830f17a8684b30017dbb33
patrickH7561 <74158152+patrickH7561@users.noreply.github.com>
2021-10-31 20:21:19 -0400

    First Attempt @ Wall Following

--- WallFollowing/src/main.cpp
+++ WallFollowing/src/main.cpp
@@ -1,11 +1,39 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <Chassis.h>
+#include <Rangefinder.h>
+#include <PIDcontroller.h>
+
+Chassis Robot;
+Rangefinder RF;
+PIDController PID(10);
+float MyDist = 0.0;
+float TargetDist = 10; 
+float BaseDrive = .5;
 
 void setup() {
-  // put your setup code here, to run once:
+  delay(1000);
+  Serial.begin(115200);
+  Robot.init();
+  RF.attach(16,17); 
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
+  RF.myGetDistance(MyDist);
+  float Effort = CalcEffort(MyDist);
+  Serial.println(Effort);
+
+  if (Effort > 0)
+  {
+    Robot.leftMotor.setEffort(BaseDrive + Effort);
+    Robot.rightMotor.setEffort(Effort);
+  }
+  else if (Effort < 0)
+  {
+    Robot.rightMotor.setEffort(Effort);
+    Robot.leftMotor.setEffort(BaseDrive + Effort);
+  }
+  else 
+  {
+    Robot.setMotorEfforts(Effort);
+  }
 }
